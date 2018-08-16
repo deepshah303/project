d859b952dd3530b67255b7bf776ee51e28bb6779
AE206_PC19 <AE206_PC19@Infva06621>
2018-08-16 08:58:15 +0530

    Spring hibernate commit 1

---
+++ HiSpring/src/entity/HibernateTest.java
@@ -0,0 +1,26 @@
+package entity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import repo.ProductRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:appctx.xml")
+public class HibernateTest {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Test
+	public void testHibernate() {
+		Product p = new Product();
+		p.setName("Hackbook");
+		p.setPrice(20000.0);
+		productRepository.add(p);
+	}
+
+}
